60fc82da5d50576816a996a1308088c79ead434c
Matheus Gonzatti <matheus.gonzatti@outlook.com>
2019-11-25 10:05:14 -0300

    Chore: First commit

---
+++ Models/Games.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace DevGames.Models
+{
+    public class Games
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [BsonElement("Name")]
+        [Required]
+        public string GameName { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string[] Plataform { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ Models/GamesstoreDatabaseSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace DevGames.Models
+{
+    public class GamesstoreDatabaseSettings : IGamesstoreDatabaseSettings
+    {
+        public string GamesCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface IGamesstoreDatabaseSettings
+    {
+        string GamesCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
